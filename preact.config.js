b3cdec21c6cdc566862bd776727a6bbfd60579b0
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-07-07 23:33:47 +0800

    netlify plugin

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,7 @@
 // prevents https://github.com/developit/preact-cli/issues/244
+const netlifyPlugin = require('preact-cli-plugin-netlify')
+
 export default config => {
-  config.node.process = 'mock';
+  config.node.process = 'mock'
+  netlifyPlugin(config)
 }
