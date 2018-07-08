afd2fe25967893045f7181c3247dd46ec29367ce
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-07-08 10:22:29 +0800

    use query component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,9 @@
+import {h, Component} from 'preact'
 import {Link} from 'preact-router/match'
-import {graphql} from 'react-apollo'
+import {Query} from 'react-apollo'
 import gql from 'graphql-tag'
 import Image from 'pimg'
+import styled from 'preact-emotion'
 
 import {
   Card,
@@ -10,38 +12,7 @@ import {
   Body as CardBody
 } from '../components/Card'
 
-const Home = ({data: {loading, error, feed, _allCategories}}) => {
-  if (error) return <h1>Error fetching feed!</h1>
-  if (loading) return <h2>Loading posts...</h2>
-
-  return (
-    <div>
-      {feed.map(post => (
-        <Card key={post.id}>
-          <CardHeader
-            title={<Link href={'/posts/' + post.id}>{post.title}</Link>}
-          >
-            <span>
-              <small>{post.pubDate}</small>
-            </span>
-          </CardHeader>
-          <CardBody>
-            <Image
-              src={post.thumbnail}
-              alt={post.title}
-              fetchOnDemand
-              width={200}
-            />
-            <p dangerouslySetInnerHTML={{__html: post.description}} />
-          </CardBody>
-          <CardFooter>{post.author}</CardFooter>
-        </Card>
-      ))}
-    </div>
-  )
-}
-
-export const feed = gql`
+export const GET_FEED = gql`
   query {
     feed {
       id
@@ -49,6 +20,8 @@ export const feed = gql`
       thumbnail
       description
       pubDate
+      author
+      categories
     }
     allCategories {
       name
@@ -56,4 +29,106 @@ export const feed = gql`
   }
 `
 
-export default graphql(feed)(Home)
+const CategorySelectClass = styled('div')`
+  margin: 20px 20px;
+`
+
+function mapCategoriesIntoOptions(categories) {
+  return categories.map(c => ({
+    value: c.name,
+    label: c.name
+  }))
+}
+
+function InputSelect({name, value, options, defaultOption, ...restProps}) {
+  return (
+    <select name={name} id={name} {...restProps}>
+      {defaultOption && <option value="">{defaultOption}</option>}
+      {options.map(option => (
+        <option value={option.value}>{option.label}</option>
+      ))}
+    </select>
+  )
+}
+
+class Home extends Component {
+  state = {
+    selectedCategory: ''
+  }
+
+  handleCategoryChange = e => {
+    this.setState({selectedCategory: e.target.value})
+  }
+
+  componentDidMount() {
+    const {category = ''} = this.props.matches
+    if (category.length > 0) {
+      this.setState({ selectedCategory: category })
+    }
+  }
+
+  render(_props, {selectedCategory}) {
+    return (
+      <div>
+        <Query query={GET_FEED}>
+          {({loading, error, data}) => {
+            if (error) return <h1>Error fetching feed!</h1>
+            if (loading) return <h2>Loading posts...</h2>
+
+            const categorySelect = (
+              <CategorySelectClass>
+                <InputSelect
+                  name="category"
+                  value={selectedCategory}
+                  defaultOption="Pilih Kategori"
+                  options={mapCategoriesIntoOptions(data.allCategories)}
+                  onChange={this.handleCategoryChange}
+                />
+              </CategorySelectClass>
+            )
+
+            return (
+              <div>
+                {categorySelect}
+                {data.feed
+                  .filter(post => {
+                    if (selectedCategory.length === 0) {
+                      return true
+                    }
+                    return post.categories.includes(selectedCategory)
+                  })
+                  .map(post => (
+                    <Card key={post.id}>
+                      <CardHeader
+                        title={
+                          <Link href={'/posts/' + post.id}>{post.title}</Link>
+                        }
+                      >
+                        <span>
+                          <small>{post.pubDate}</small>
+                        </span>
+                      </CardHeader>
+                      <CardBody>
+                        <Image
+                          src={post.thumbnail}
+                          alt={post.title}
+                          fetchOnDemand
+                          width={200}
+                        />
+                        <p
+                          dangerouslySetInnerHTML={{__html: post.description}}
+                        />
+                      </CardBody>
+                      <CardFooter>{post.author}</CardFooter>
+                    </Card>
+                  ))}
+              </div>
+            )
+          }}
+        </Query>
+      </div>
+    )
+  }
+}
+
+export default Home
--- src/routes/Post.js
+++ src/routes/Post.js
@@ -1,4 +1,4 @@
-import {graphql} from 'react-apollo'
+import {Query} from 'react-apollo'
 import gql from 'graphql-tag'
 
 import {
@@ -8,28 +8,7 @@ import {
   Body as CardBody
 } from '../components/Card'
 
-const Post = ({data: {loading, error, post}}) => {
-  if (error) return <h1>Error fetching post!</h1>
-  if (loading) return <h2>Loading posts...</h2>
-
-  return (
-    <div>
-      <Card key={post.id}>
-        <CardHeader title={<h3>{post.title}</h3>}>
-          <span>
-            <small>{post.pubDate}</small>
-          </span>
-        </CardHeader>
-        <CardBody>
-          <div dangerouslySetInnerHTML={{__html: post.content}} />
-        </CardBody>
-        <CardFooter>{post.author}</CardFooter>
-      </Card>
-    </div>
-  )
-}
-
-const q = gql`
+const GET_POST = gql`
   query post($id: ID!) {
     post(id: $id) {
       id
@@ -41,8 +20,31 @@ const q = gql`
   }
 `
 
-export default graphql(q, {
-  options: ({id}) => ({
-    variables: {id}
-  })
-})(Post)
+const Post = ({id}) => (
+  <div>
+    <Query query={GET_POST} variables={{id}}>
+      {({loading, error,data}) => {
+        if (error) return <h1>Error fetching post!</h1>
+        if (loading) return <h2>Loading posts...</h2>
+
+        const {post} = data
+
+        return (
+          <Card key={post.id}>
+            <CardHeader title={<h3>{post.title}</h3>}>
+              <span>
+                <small>{post.pubDate}</small>
+              </span>
+            </CardHeader>
+            <CardBody>
+              <div dangerouslySetInnerHTML={{__html: post.content}} />
+            </CardBody>
+            <CardFooter>{post.author}</CardFooter>
+          </Card>
+        )
+      }}
+    </Query>
+  </div>
+)
+
+export default Post
